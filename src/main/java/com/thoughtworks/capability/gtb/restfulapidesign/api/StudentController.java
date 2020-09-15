40cb20c0671aad7a104a6b604a608b0fd1e3f354
Yang Siyu <siyuyang1995@gmail.com>
2020-09-15 09:25:31 +0800

    [yang siyu]refactor: refactor controller

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/StudentController.java
@@ -4,41 +4,42 @@ import com.thoughtworks.capability.gtb.restfulapidesign.domain.Student;
 import com.thoughtworks.capability.gtb.restfulapidesign.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1/students")
 public class StudentController {
     @Autowired
     StudentService studentService;
 
-    @PostMapping("/students")
-    public ResponseEntity addStudent(@RequestBody Student student) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addStudent(@RequestBody Student student) {
         studentService.addStudents(student);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @DeleteMapping("/students/{id}")
-    public ResponseEntity delStudent(@PathVariable Integer id) {
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delStudent(@PathVariable Integer id) {
         studentService.delStudent(id);
-        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping("/students")
-    public ResponseEntity<List> getStudentsByGender(@RequestParam(required = false) String gender) {
-        return ResponseEntity.status(HttpStatus.OK).body(studentService.getStudentsByGender(gender));
+    @ResponseStatus(HttpStatus.OK)
+    public List getStudentsByGender(@RequestParam(required = false) String gender) {
+        return studentService.getStudentsByGender(gender);
     }
 
-    @GetMapping("/students/{id}")
-    public ResponseEntity<Student> getStudentsById(@PathVariable int id) {
-        return ResponseEntity.status(HttpStatus.OK).body(studentService.getStudentsById(id));
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Student getStudentsById(@PathVariable int id) {
+        return studentService.getStudentsById(id);
     }
 
     @PatchMapping("/students/{id}")
-    public ResponseEntity<Student> updateStudentInfo(@PathVariable int id, @RequestBody Student studentInfo) {
-        return ResponseEntity.status(HttpStatus.OK).body(studentService.updateStudentInfo(id, studentInfo));
+    public Student updateStudentInfo(@PathVariable int id, @RequestBody Student studentInfo) {
+        return studentService.updateStudentInfo(id, studentInfo);
     }
-
 }
--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/TeamController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/TeamController.java
@@ -4,23 +4,25 @@ import com.thoughtworks.capability.gtb.restfulapidesign.domain.Team;
 import com.thoughtworks.capability.gtb.restfulapidesign.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1/teams")
 public class TeamController {
     @Autowired
     TeamService teamService;
 
-    @GetMapping("/teams")
-    public ResponseEntity<List> getTeam() {
-        return ResponseEntity.status(HttpStatus.OK).body(teamService.splitIntoTeams());
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Team> getTeam() {
+        return teamService.splitIntoTeams();
     }
 
-    @PatchMapping("/teams/{id}")
-    public ResponseEntity<Team> updateTeamName(@PathVariable int id, @RequestParam String teamName) {
-        return ResponseEntity.status(HttpStatus.OK).body(teamService.updateTeamName(id, teamName));
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Team updateTeamName(@PathVariable int id, @RequestParam String teamName) {
+        return teamService.updateTeamName(id, teamName);
     }
 }
